ed8c91cd401925e7ff88a40773e029de193d0d32
jerk88 <jerk88@gazeta.pl>
2011-09-10 09:45:41 +0200

    Added cart support

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Depot::Application.routes.draw do
-  resources :products
+  resources :line_items
 
+  resources :carts
+
+  get "store/index"
+
+  resources :products
+  
+  root :to => 'store#index', :as => 'store'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
