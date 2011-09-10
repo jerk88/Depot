8073e5c600eb7fb9e6bdf0297531e0cefd6ac408
jerk88 <jerk88@gazeta.pl>
2011-09-10 13:38:21 +0200

    added mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+
+
+Depot::Application.configure do
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+              :address => "smtp.gmail.com",
+              :port => 587,
+              :domain => "depot.com",
+              :authentication => "plain",
+              :user_name => "stanczuk.artur",
+              :password => "zaq123ewq",
+              :enable_starttls_auto => true
+}
+end
